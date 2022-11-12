b73f85e5f15cba5e3dd616ba24f394c1eee92ccf
sr-it-timespro <sr.it.timespro@gmail.com>
2022-11-12 14:57:10 +0530

    Files

---
+++ 01-session/todo-app-v1/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {TODOManager} from "./components/todo-manager"
+
+function App() {
+  return (
+    <div className="App">
+      TODO Manager Application
+
+      <TODOManager></TODOManager>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-session/todo-app-v1/client/src/components/todo-manager.tsx
@@ -0,0 +1,12 @@
+
+const TODOManager = () => {
+
+  return (
+    <div>
+      TODO Manager Component
+    </div>
+  )
+}
+
+export {TODOManager};
+
