75aae8d32e4d128ed191cacacbed28ac3fa49888
sr-it-timespro <sr.it.timespro@gmail.com>
2022-11-13 14:05:54 +0530

    TODO App - Handline Error

--- 01-session/todo-app-v1/client/src/components/todo-manager.tsx
+++ 01-session/todo-app-v1/client/src/components/todo-manager.tsx
@@ -3,23 +3,31 @@ import { useEffect, useState } from "react";
 import ITodoItem from "../models/todo-model";
 import {getAllTODOItems} from "../services/todo-service"
 import {TodoItems} from "./todo-items"
-import {Container} from "react-bootstrap";
+import {Container, Alert} from "react-bootstrap";
 
 const TODOManager = () => {
 
   const [todoItems, setTodoItems] = useState<ITodoItem[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+
 
   useEffect(() => {
 
       const getAllTODOItemsInvoker = async () => {
         
-        const response = await getAllTODOItems();
-        console.log(response);
-        setTodoItems(response);
+        try {
+          const response = await getAllTODOItems();
+          console.log(response);
+          setTodoItems(response);  
+        }catch (errorObject){
+          // Control comes here for error
+          setError(errorObject as Error);
+        }
       }
       getAllTODOItemsInvoker();
 
   }, [])
+
 
   return (
     <Container>
@@ -28,7 +36,21 @@ const TODOManager = () => {
         TODO Manager Application
       </h2>
       
-      <TodoItems todoItems={todoItems}></TodoItems>
+      {
+        error &&  (
+          <Alert key="danger" variant="danger">
+            {error.message}
+          </Alert>
+        )
+      }
+
+      {
+        !error && (
+          <TodoItems todoItems={todoItems}></TodoItems>
+        )
+      }
+
+      
 
     </Container>
   )
