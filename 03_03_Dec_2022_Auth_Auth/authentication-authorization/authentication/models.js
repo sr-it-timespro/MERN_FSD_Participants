b6c9f740db31cf0f2eee2be54620ecc49c0ad8c2
sr-it-timespro <sr.it.timespro@gmail.com>
2022-12-06 19:47:08 +0530

    Sign-up - Part II - Mongoose Hooks

--- 03_03_Dec_2022_Auth_Auth/authentication-authorization/authentication/models.js
+++ 03_03_Dec_2022_Auth_Auth/authentication-authorization/authentication/models.js
@@ -15,6 +15,24 @@ const userSchema = new mongoose.Schema({
   }
 })
 
+userSchema.pre('save', function(next) {
+
+  console.log('Method/Hooked called before save...')
+  console.log("Meaning of this " + this);
+
+  next();
+})
+
+userSchema.post('save', function(userDocument, next) {
+
+  console.log('Method/Hook called after the save operation...')
+  console.log("Meaning of this " + this);
+  console.log("USer document " + userDocument);
+
+  next();
+})
+
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
